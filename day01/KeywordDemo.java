86c16475e9fb656ac4e635b5cd3a6748e9ad355d
MRsLI <17861509590@163.com>
2020-10-24 11:47:44 +0800

    javase第一天

---
+++ day01/KeywordDemo.java
@@ -0,0 +1,11 @@
+public class  KeywordDemo{
+	public  static void main (String [] args){
+		System.out.println("helloword");
+		System.out.println(123);
+		System.out.println('q');
+		System.out.print(false);
+		System.out.print(1.22);
+		//System.out.println(null);
+
+	}
+}
---
+++ day01/VariableDemo.java
@@ -0,0 +1,33 @@
+public class VariableDemo {
+	public static void main (String [] args){
+		//整型变量
+		byte num1 = 100;
+		short num2 = 1000;
+		int num3 = 1234567890;
+		long num4 = 12345678900L;
+
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+
+		//浮点型变量
+		float num5 = 1.21f;
+		double num6 = 3.14;
+
+		System.out.println(num5);
+		System.out.println(num6);
+
+		//字符型变量
+		char b = 'a';
+
+		System.out.println(b);
+
+		//布尔型变量
+		boolean T = false;
+
+		System.out.println(T);
+
+
+	}
+}
